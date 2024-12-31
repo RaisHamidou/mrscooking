bca7ce088fadf307a020ce3164452061948d5adf
Adel <numidian.dev@gmail.com>
2024-12-31 21:48:48 +0100

    test

--- src/app/ebooks/page.js
+++ src/app/ebooks/page.js
@@ -3,16 +3,7 @@ import Cards from "@/components/Cards/Cards";
 import URL from "@/components/config/config";
 
 export default async function Page() {
-  let data = await axios.get(`${URL}/api/books`, {
-    method: "GET",
-    headers: {
-      Authorization: "bejaia1984",
-      Accept: "Application/json",
-    },
-  });
-  let posts = await data.data;
 
-  console.log(posts);
   return (
     <section>
       <div className="books-page-hero">
