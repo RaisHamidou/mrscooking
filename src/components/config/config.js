e1bc8a9f8d95627316942da817c1eb97c3387b7d
Adel <numidian.dev@gmail.com>
2024-12-31 21:44:47 +0100

    test

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV != "production"
-const URL = dev ? "http://localhost:4000":"https://www.mrscooking.com"
+const URL = dev ? "http://localhost:4000":"https://mrscooking-git-main-raishamidou1s-projects.vercel.app"
 
 export default URL
