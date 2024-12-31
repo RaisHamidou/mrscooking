2a4a5ff6b56bd90ddbe08bd55956ce1f83dc10d5
Adel <numidian.dev@gmail.com>
2024-12-31 21:42:25 +0100

    test

--- src/app/ebooks/[book]/page.js
+++ src/app/ebooks/[book]/page.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import Details from "@/components/Details/Details";
+import URL from "@/components/config/config";
 
 export default async function Page({ params }) {
   const { book } = params;
-  let data = await axios.get(`http://localhost:4000/api/books/${book}`);
+  let data = await axios.get(`${URL}/api/books/${book}`);
   let posts = await data.data;
   return (
     <section className="container-detail-book">
