23af4898abb4e09ce76977462036f5980cc8623d
Adel <numidian.dev@gmail.com>
2025-01-01 02:51:29 +0100

    endpoint

--- backend/data/books.js
+++ backend/data/books.js
@@ -8,7 +8,7 @@ const books = [
       "Retrouvez 10 recettes de desserts incontournables et faciles à réaliser pour vous régaler avec Mr. E-book détaillé et bien expliqué afin de permettre à chacune d’entre vous de réussir votre tête-à-tête avec Mr.",
     detail: { pages: 35, format: "PDF", langue: "Français" },
     image: "/image/book/book1.png",
-    apiEndpoint: "/api/books/1",
+    apiEndpoint: "https://www.mrscooking.com/api/book/10%20recettes%20de%20desserts",
     pdfPath: "./backend/Books/10 recettes de desserts.pdf",
   },
  
