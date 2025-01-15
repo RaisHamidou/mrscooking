6a11a27c3d1c02fc4cdcfe89a88e35facb422c13
Adel <numidian.dev@gmail.com>
2025-01-15 16:23:25 +0100

    update price

--- backend/data/books.js
+++ backend/data/books.js
@@ -3,7 +3,7 @@ const books = [
   {
     id: 1,
     titre: "Ebook 1 : 10 recettes de desserts édition tête-à-tête avec Mr",
-    price: 7.99,
+    price: 6.99,
     originalPrice: 7.99,
     description:
       "<p>Retrouvez 10 recettes de desserts incontournables et faciles à réaliser pour vous régaler avec Mr. E-book détaillé et bien expliqué afin de permettre à chacune d’entre vous de réussir votre tête-à-tête avec Mr.</p> <br> <p>Dans cet E-book tu trouveras 10 recettes incontournables pour un tête à tête :</p> <ul> <li>Chantilly mascarpone (bonus) </li> <li>Pavlova </li> <li>Verrine citronnée</li> <li>Coulant au chocolat </li> <li>Dôme tiramisu</li> <li>Crème brûlée</li> <li>Moelleux aux pommes</li> <li>Milkshake vanille caramel </li> <li>Panacota</li> <li>Entremet chocolat</li> <li>Mini charlotte aux framboises</li> </ul>",
@@ -17,9 +17,9 @@ const books = [
   {
     id: 2,
     titre: "E-book 2 : 10 recettes de plat comme au resto",
-    price: 9.99,
+    price: 8.99,
     originalPrice: 9.99,
-    description:"<p>Retrouvez 10 recettes de plat incontournables et faciles à réaliser pour vous régaler avec Mr. E-book détaillé et bien expliqué afin de permettre à chacune d’entre vous de réussir votre tête-à-tête avec Mr.</p> <br> <p>Dans cet E-book tu trouveras 10 recettes simple et délicieuses pour un tête à tête :</p> <ul> <li>Souris d’agneau et son écrasé de patate douce </li> <li>Coquille Saint-Jacques accompagné de sa purée</li> <li>Escalope farcie au cheddar et épinard</li> <li>Moules à la crème</li> <li>Patate douce rôties et saumon tyriaki</li> <li>Courgettes farcies à la viande hachée</li> <li>Saumon rôti et blé sautés aux champignons</li> <li>Courge jack be little et cabillaud grillé</li> <li>Tagine de chou-fleur</li> <li>Carottes  et côtelettes rôties</li> </ul>",
+    description:"<p>Retrouvez 10 recettes de plat incontournables et faciles à réaliser pour vous régaler avec Mr. E-book détaillé et bien expliqué afin de permettre à chacune d’entre vous de réussir votre tête-à-tête avec Mr.</p> <br> <p>Dans cet E-book tu trouveras 10 recettes simple et délicieuses pour un tête à tête :</p> <ul> <li>Souris d’agneau et son écrasé de patate douce </li> <li>Coquille Saint-Jacques accompagnée de sa purée</li> <li>Escalope farcie au cheddar et épinard</li> <li>Moules à la crème</li> <li>Patate douce rôties et saumon tyriaki</li> <li>Courgettes farcies à la viande hachée</li> <li>Saumon rôti et blé sautés aux champignons</li> <li>Courge jack be little et cabillaud grillé</li> <li>Tagine de chou-fleur</li> <li>Carottes  et côtelettes rôties</li> </ul>",
     detail: { pages: 35, format: "PDF", langue: "Français" },
     image: "/image/book/ebook2.png",
     image_books:["/image/photo/ebook 2/first.jpg","/image/photo/ebook 2/1.jpg","/image/photo/ebook 2/2.jpg","/image/photo/ebook 2/3.png","/image/photo/ebook 2/4.jpg","/image/photo/ebook 2/5.jpg"],
@@ -30,7 +30,7 @@ const books = [
   {
     id: 3,
     titre: "Pack desserts et plats",
-    price: 12.99,
+    price: 11.99,
     originalPrice: 12.99,
     description:"<p>Un pack avec les deux premiers E-books recettes desserts et plats pour vous régaler. Des recettes incontournables et faciles à réaliser.</p> <br><p>Dans l’E-book plats comme au resto tu trouveras 10 recettes simples et délicieuses pour un tête-à-tête :</p> <ul> <li>Souris d’agneau et son écrasé de patate douce </li> <li>Coquille Saint-Jacques accompagnée de sa purée</li> <li>Escalope farcie au cheddar et épinard</li> <li>Moules à la crème</li> <li> Patates douces rôties et saumon tyriaki</li> <li>Courgettes farcies à la viande hachée </li> <li>Saumon rôti et blé sautés aux champignons</li> <li>Courge jack be little et cabillaud grillé </li> <li>Tagine de chou-fleur</li> <li> Carottes  et côtelettes rôties</li> </ul> <br> <p>Dans l’E-book dessers tu trouveras 10 recettes de desserts légers à déguster après un bon repas :</p>  <ul> <li>Chantilly mascarpone (bonus)</li> <li>Pavlova</li> <li>Verrine citronnée</li> <li>Coulant au chocolat</li> <li>Dôme tiramisu</li> <li>Crème brûlée</li> <li>Moelleux aux pommes</li> <li>Milkshake vanille caramel</li> <li>Panacota</li> <li>Entremet chocolat</li> <li>Mini charlotte aux framboises</li> </ul>",
     detail: { pages: 35, format: "PDF", langue: "Français" },
